390b9dddce714abf7426baef3064e06bcc6d97b6
C-D-Lewis <Bonsitm@gmail.com>
2015-04-27 16:13:15 -0700

    Add submit item

--- radio-button/src/main.c
+++ radio-button/src/main.c
@@ -15,30 +15,36 @@ static MenuLayer *s_menu_layer;
 static int s_current_selection = 0;
 
 static uint16_t get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *context) {
-  return NUM_ROWS;
+  return NUM_ROWS + 1;
 }
 
 static void draw_row_callback(GContext *ctx, Layer *cell_layer, MenuIndex *cell_index, void *context) {
-  static char s_buff[16];
-  snprintf(s_buff, sizeof(s_buff), "Choice %d", (int)cell_index->row);
-  menu_cell_basic_draw(ctx, cell_layer, s_buff, NULL, NULL);
-
-  GRect bounds = layer_get_bounds(cell_layer);
-  GPoint p = GPoint(bounds.size.w - (3 * RADIO_RADIUS), (bounds.size.h / 2));
-
-  // Selected?
-  if(menu_cell_layer_is_highlighted(cell_layer)) {
-    graphics_context_set_stroke_color(ctx, GColorWhite);
-    graphics_context_set_fill_color(ctx, GColorWhite);
+  if(cell_index->row == NUM_ROWS) {
+    // This is the submit item
+    menu_cell_basic_draw(ctx, cell_layer, "Submit", NULL, NULL);
   } else {
-    graphics_context_set_fill_color(ctx, GColorBlack);
-  }
+    // This is a choice item
+    static char s_buff[16];
+    snprintf(s_buff, sizeof(s_buff), "Choice %d", (int)cell_index->row);
+    menu_cell_basic_draw(ctx, cell_layer, s_buff, NULL, NULL);
 
-  // Draw radio
-  graphics_draw_circle(ctx, p, RADIO_RADIUS);
-  if(cell_index->row == s_current_selection) {
-    // This is the selection
-    graphics_fill_circle(ctx, p, RADIO_RADIUS - 3);
+    GRect bounds = layer_get_bounds(cell_layer);
+    GPoint p = GPoint(bounds.size.w - (3 * RADIO_RADIUS), (bounds.size.h / 2));
+
+    // Selected?
+    if(menu_cell_layer_is_highlighted(cell_layer)) {
+      graphics_context_set_stroke_color(ctx, GColorWhite);
+      graphics_context_set_fill_color(ctx, GColorWhite);
+    } else {
+      graphics_context_set_fill_color(ctx, GColorBlack);
+    }
+
+    // Draw radio filled/empty
+    graphics_draw_circle(ctx, p, RADIO_RADIUS);
+    if(cell_index->row == s_current_selection) {
+      // This is the selection
+      graphics_fill_circle(ctx, p, RADIO_RADIUS - 3);
+    }
   }
 }
 
@@ -47,8 +53,14 @@ static int16_t get_cell_height_callback(struct MenuLayer *menu_layer, MenuIndex 
 }
 
 static void select_callback(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context) {
-  s_current_selection = cell_index->row;
-  menu_layer_reload_data(menu_layer);
+  if(cell_index->row == NUM_ROWS) {
+    // Do something with user choice
+    APP_LOG(APP_LOG_LEVEL_INFO, "Submitted choice %d", s_current_selection);
+  } else {
+    // Change selection
+    s_current_selection = cell_index->row;
+    menu_layer_reload_data(menu_layer);
+  }
 }
 
 static void window_load(Window *window) {
