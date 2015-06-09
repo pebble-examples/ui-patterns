295f3c3daa8e7fe47e9d4aa0046ee1fd188179a3
C-D-Lewis <Bonsitm@gmail.com>
2015-06-08 17:11:37 -0700

    Update progress bar pattern

--- src/windows/progress_bar_window.c
+++ src/windows/progress_bar_window.c
@@ -16,12 +16,9 @@ static void progress_callback(void *context);
 static void progress_bar_proc(Layer *layer, GContext *ctx) {
   GRect bounds = layer_get_bounds(layer);
 
-  graphics_context_set_fill_color(ctx, GColorDarkCandyAppleRed);
-  graphics_fill_rect(ctx, bounds, GCornerNone, 0);
-
   int width = (int)(float)(((float)s_progress / 100.0F) * bounds.size.w);
-  graphics_context_set_fill_color(ctx, GColorMelon);
-  graphics_fill_rect(ctx, GRect(0, 0, width, bounds.size.h), GCornerNone, 0);
+  graphics_context_set_stroke_color(ctx, GColorWhite);
+  graphics_draw_line(ctx, GPointZero, GPoint(width, 0));
 }
 
 static void next_timer() {
@@ -29,10 +26,9 @@ static void next_timer() {
 }
 
 static void progress_callback(void *context) {
-  if(++s_progress <= 100) {
-    layer_mark_dirty(s_progress_bar);
-    next_timer();
-  }
+  s_progress += (s_progress < 100) ? 1 : -100;
+  layer_mark_dirty(s_progress_bar);
+  next_timer();
 }
 
 static void window_appear(Window *window) {
@@ -44,14 +40,17 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_progress_bar = layer_create((GRect){ .size = PROGRESS_BAR_WINDOW_SIZE });
-  layer_set_update_proc(s_progress_bar, progress_bar_proc);
-  layer_add_child(window_layer, s_progress_bar);
-
   s_status_bar = status_bar_layer_create();
   status_bar_layer_set_separator_mode(s_status_bar, StatusBarLayerSeparatorModeDotted);
-  status_bar_layer_set_colors(s_status_bar, GColorClear, GColorBlack);
+  status_bar_layer_set_colors(s_status_bar, GColorClear, GColorWhite);
   layer_add_child(window_layer, status_bar_layer_get_layer(s_status_bar));
+
+  s_progress_bar = layer_create((GRect){ 
+    .origin = GPoint(0, STATUS_BAR_LAYER_HEIGHT - 2), 
+    .size = PROGRESS_BAR_WINDOW_SIZE 
+  });
+  layer_set_update_proc(s_progress_bar, progress_bar_proc);
+  layer_add_child(window_layer, s_progress_bar);
 }
 
 static void window_unload(Window *window) {
@@ -71,7 +70,7 @@ static void window_disappear(Window *window) {
 void progress_bar_window_push() {
   if(!s_window) {
     s_window = window_create();
-    window_set_background_color(s_window, GColorFolly);
+    window_set_background_color(s_window, GColorDarkCandyAppleRed);
     window_set_window_handlers(s_window, (WindowHandlers) {
       .appear = window_appear,
       .load = window_load,
--- src/windows/progress_bar_window.h
+++ src/windows/progress_bar_window.h
@@ -2,7 +2,7 @@
 
 #include <pebble.h>
 
-#define PROGRESS_BAR_WINDOW_SIZE GSize(144, STATUS_BAR_LAYER_HEIGHT) // System default
+#define PROGRESS_BAR_WINDOW_SIZE GSize(144, 1) // System default
 #define PROGRESS_BAR_WINDOW_DELTA 33
 
 void progress_bar_window_push();
