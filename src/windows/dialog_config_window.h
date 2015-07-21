9b674dd758839fe6b083cbfcbc039c6f14e171f5
C-D-Lewis <Bonsitm@gmail.com>
2015-07-21 13:50:59 -0700

    Improve config window

--- src/windows/dialog_config_window.h
+++ src/windows/dialog_config_window.h
@@ -3,6 +3,6 @@
 #include <pebble.h>
 
 #define DIALOG_CONFIG_WINDOW_APP_NAME "Example App"
-#define DIALOG_CONFIG_WINDOW_MESSAGE  "Set up in the Pebble app"
+#define DIALOG_CONFIG_WINDOW_MESSAGE  "Set up in the\nPebble app"
 
 void dialog_config_window_push();
