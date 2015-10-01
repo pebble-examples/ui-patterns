c4a5a19d9199b2b17ecf37db46e3dbe24627c654
Chris Lewis <Bonsitm@gmail.com>
2015-10-01 10:01:33 -0700

    Updated main menu

--- src/main.c
+++ src/main.c
@@ -11,7 +11,9 @@
 #include "windows/progress_layer_window.h"
 #include "windows/dialog_config_window.h"
 
-#define NUM_WINDOWS 10
+#define NUM_WINDOWS                10
+#define FOCUSED_TALL_CELL_HEIGHT   60
+#define UNFOCUSED_TALL_CELL_HEIGHT 30
 
 static Window *s_main_window;
 static MenuLayer *s_menu_layer;
@@ -58,7 +60,13 @@ static void draw_row_callback(GContext *ctx, Layer *cell_layer, MenuIndex *cell_
 }
 
 static int16_t get_cell_height_callback(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *context) {
-  return CHECKBOX_WINDOW_CELL_HEIGHT;
+#ifdef PBL_ROUND
+  MenuIndex selected = menu_layer_get_selected_index(menu_layer);
+  return selected.row == cell_index->row && selected.section == cell_index->section ? 
+    FOCUSED_TALL_CELL_HEIGHT : UNFOCUSED_TALL_CELL_HEIGHT;
+#else
+  return 44;
+#endif
 }
 
 static void pin_complete_callback(PIN pin, void *context) {
@@ -107,32 +115,18 @@ static void select_callback(struct MenuLayer *menu_layer, MenuIndex *cell_index,
   }
 }
 
-static void draw_header_callback(GContext *ctx, const Layer *cell_layer, uint16_t section_index, void *context) {
-  menu_cell_basic_header_draw(ctx, cell_layer, "Choose a Component");
-}
-
-static int16_t get_header_height_callback(struct MenuLayer *menu_layer, uint16_t section_index, void *context) {
-  return MENU_CELL_BASIC_HEADER_HEIGHT;
-}
-
-static uint16_t get_num_sections_callback(struct MenuLayer *menu_layer, void *context) {
-  return 1;
-}
-
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
   s_menu_layer = menu_layer_create(bounds);
   menu_layer_set_click_config_onto_window(s_menu_layer, window);
+  menu_layer_set_center_focused(s_menu_layer, PBL_IF_ROUND_ELSE(true, false));
   menu_layer_set_callbacks(s_menu_layer, NULL, (MenuLayerCallbacks) {
       .get_num_rows = (MenuLayerGetNumberOfRowsInSectionsCallback)get_num_rows_callback,
       .draw_row = (MenuLayerDrawRowCallback)draw_row_callback,
       .get_cell_height = (MenuLayerGetCellHeightCallback)get_cell_height_callback,
       .select_click = (MenuLayerSelectCallback)select_callback,
-      .draw_header = (MenuLayerDrawHeaderCallback)draw_header_callback,
-      .get_header_height = (MenuLayerGetHeaderHeightCallback)get_header_height_callback,
-      .get_num_sections = (MenuLayerGetNumberOfSectionsCallback)get_num_sections_callback,
   });
   layer_add_child(window_layer, menu_layer_get_layer(s_menu_layer));
 }
