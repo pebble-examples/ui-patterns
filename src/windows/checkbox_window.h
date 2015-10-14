0d773756dc13c5ae7fabff87d92d160825c9af95
Chris Lewis <Bonsitm@gmail.com>
2015-10-14 13:39:25 -0700

    Use GEdgeInsets for config window

--- src/windows/checkbox_window.h
+++ src/windows/checkbox_window.h
@@ -2,7 +2,7 @@
 
 #include <pebble.h>
 
-#define CHECKBOX_WINDOW_NUM_ROWS    4
-#define CHECKBOX_WINDOW_BOX_SIZE    12
+#define CHECKBOX_WINDOW_NUM_ROWS 4
+#define CHECKBOX_WINDOW_BOX_SIZE 12
 
 void checkbox_window_push();
